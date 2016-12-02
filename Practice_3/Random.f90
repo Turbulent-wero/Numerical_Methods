112444eba452695d855a461b3ec9054284a8a8dd
eleve <eleve@E505PC08.priv.ec-lille.fr>
2016-12-02 19:22:43 +0100

    Changes to Thomas algorithm in all folders

--- Practice_3/Random.f90
+++
@@ -1,8 +0,0 @@
-program test_random_number
-            INTEGER :: i
-            REAL,DIMENSION(0:4) :: r
-            CALL RANDOM_NUMBER(r)
-            do i=0,4
-              print*,r(i)
-            end do
-          end program
