76fcff169f8d3c123db2cc25b2a0599e7a30deaa
eleve <eleve@E505PC08.priv.ec-lille.fr>
2016-12-05 20:04:41 +0100

    fuga

--- Practice_4/Dirichlet.f90
+++ Practice_4/Dirichlet.f90
@@ -1,65 +1,70 @@
 PROGRAM Dirichlet
   IMPLICIT NONE
-  Integer*8 :: N,cont=10
-  Real*8 :: dx,pi
+  INTEGER :: N,M,j,counter=10
+  REAL*8 :: pi,dx
+  REAL*8,DIMENSION(0:100000) :: x,a,b,c,d,U,Uex,f
   OPEN(UNIT=1,FILE="Dirichlet.txt",ACTION="WRITE")
-!   read(*,*)N
-  do N=10,10000,10
-  if(N==cont) then
-    pi = 4.d0*datan(1.d0)
-    dx = pi/N
-    call algorithm(N,dx)
-    cont=cont*10
-  end if
-  end do
-! call error
-CONTAINS
-  SUBROUTINE algorithm(N,dx)
-    INTEGER*8,INTENT(IN) :: N
-    REAL*8,INTENT(IN) :: dx
-    INTEGER :: i
-    REAL*8,DIMENSION(0:N) :: x,a,b,c,d,U,Uex,f
-    REAL*8 :: error
-    U(0) = 1
-    U(N) = -1
-    do i=0,N
-      x(i) = i*dx			!Mesh
-      Uex(i) = cos(x(i))		!Exact Solution
+  pi = 4.d0*datan(1.d0)
+  do M=10,10000,10
+    if(M==counter) then
+    dx = pi/M
+    do j=0,M		
+      x(j) = j*dx		!Mesh
+      Uex(j) = cos(x(j))	!Exact solution
+      f(j) = -cos(x(j))
     end do
-    do i=1,N-1
-      f(i) = -cos(x(i))			!Value of f(i)
-      a(i) = 1				!Set or Matrix "A"
-      b(i) = -2	
-      c(i) = 1	
-      if(i==1) then			!Right hand side "d"
-        d(i) = (dx**2)*(f(i))-U(0)
-        else if(i==N-1) then
-          d(i) = (dx**2)*(f(i))-U(N)
+    U(0) = 1		!Lower boundary condition
+    U(M) = -1		!Upper boundary condition         	
+    do j=1,M-1 		!Matrix
+      if(j==1) then
+        b(j) = -2
+        c(j) = 1
+        d(j) = (dx**2)*f(j)-U(0)
+        else if(j==M-1) then
+          a(j) = 1
+          b(j) = -2
+          d(j) = (dx**2)*f(j)-U(M)
         else
-          d(i) = (dx**2)*(f(i))
+          a(j) = 1
+	  b(j) = -2
+  	  c(j) = 1  
+  	  d(j) = (dx**2)*f(j)
       end if
     end do
-!    write(*,*) d(1),d(2),d(N-1)
-!Thomas Algorithm    
-    do i=2,N-1
-      b(i) = b(i) - (a(i)/b(i-1))*c(i-1)
-      d(i) = d(i) - (a(i)/b(i-1))*d(i-1)
-!      write(*,*)a(i),b(i)
-    end do
-    U(N-1) = d(N-1)/b(N-1)
-    do i=N-2,1,-1
-      U(i) = (d(i)-c(i)*U(i+1))/b(i)
-    end do
-!Error
-    error = 0
-    do i=0,N
-      error = error + (Uex(i) - U(i))**2
-    end do
-    error = (dx*error)**.5
-    write(1,*) log(dx/pi),log(error)
-!    write(1,*) dx/pi,error
-  END SUBROUTINE algorithm
-
+    N=M-1
+    call Thomas(U,a,b,c,d,N,j)	!Call Thomas algorithm subroutine
+    call error(j,M,U,Uex,dx)	!Call error subroutine
+    counter=counter*10
+    end if
+  end do
+CONTAINS
+    SUBROUTINE Thomas(U,a,b,c,d,N,j)
+      INTEGER,INTENT(INOUT) :: N,j
+      REAL*8,DIMENSION(0:1000),INTENT(INOUT) :: U,a,b,c,d
     
-
+      do j=2,N
+        b(j) = b(j) - (a(j)/b(j-1))*c(j-1)
+        d(j) = d(j) - (a(j)/b(j-1))*d(j-1)
+      end do
+      U(N) = d(N)/b(N)
+      do j=N-1,1,-1
+        U(j) = (d(j)-c(j)*U(j+1))/b(j)
+      end do
+!do j=1,N
+! write(1,*) "x(",j,") = ",x(j),U(j),Uex(j)
+!end do
+    END SUBROUTINE Thomas
+    SUBROUTINE error(j,M,U,Uex,dx)
+      INTEGER,INTENT(INOUT) :: j
+      INTEGER,INTENT(IN) :: M
+      REAL*8,INTENT(INOUT) :: dx
+      REAL*8,DIMENSION(0:1000),INTENT(INOUT) :: U,Uex
+      REAL*8 :: e
+      e=0
+      do j=0,M
+	e = e + (abs(Uex(j)-U(j)))**2
+      end do
+      e = (e*dx)**.5
+      write(1,*) dx/pi,e
+    END SUBROUTINE error
 END PROGRAM Dirichlet
--- Practice_4/Newman.f90
+++ Practice_4/Newman.f90
@@ -1,40 +1,39 @@
-PROGRAM Dirichlet
+PROGRAM Newmann
   IMPLICIT NONE
   INTEGER :: N,M,j,counter=10
   REAL*8 :: pi,dx
   REAL*8,DIMENSION(0:100000) :: x,a,b,c,d,U,Uex,f
-  OPEN(UNIT=1,FILE="Dirichlet.txt",ACTION="WRITE")
+  OPEN(UNIT=1,FILE="Newmann.txt",ACTION="WRITE")
   pi = 4.d0*datan(1.d0)
   do M=10,10000,10
     if(M==counter) then
     dx = pi/M
     do j=0,M		
       x(j) = j*dx		!Mesh
-      Uex(j) = cos(x(j))	!Exact solution
-      f(j) = -cos(x(j))
-    end do
-    U(0) = 1		!Lower boundary condition
-    U(M) = -1		!Upper boundary condition         	
-    do j=1,M-1 		!Matrix
-      if(j==1) then
+      Uex(j) = cos(pi*x(j))	!Exact solution
+      f(j) = ((-pi)**2)*(cos(pi*x(j)))
+    end do     	
+    do j=0,M	 		!Matrix
+      if(j==0) then
         b(j) = -2
-        c(j) = 1
-        d(j) = (dx**2)*f(j)-U(0)
-        else if(j==M-1) then
-          a(j) = 1
-          b(j) = -2
-          d(j) = (dx**2)*f(j)-U(M)
+        c(j) = 2
+        d(j) = (dx**2)*f(j)
+        else if(j==M) then
+          a(j) = 2
+          b(j) = -2*(1+dx)
+          d(j) = (dx**2)*f(j)-2*dx
         else
           a(j) = 1
 	  b(j) = -2
   	  c(j) = 1  
   	  d(j) = (dx**2)*f(j)
       end if
+write(1,*) a(j),b(j),c(j),d(j)
     end do
-    N=M-1
+    N=M+1
     call Thomas(U,a,b,c,d,N,j)	!Call Thomas algorithm subroutine
     call error(j,M,U,Uex,dx)	!Call error subroutine
-    counter=counter*10
+    !counter=counter*10
     end if
   end do
 CONTAINS
@@ -48,19 +47,23 @@ CONTAINS
       end do
       U(N) = d(N)/b(N)
       do j=N-1,1,-1
-        U(j) = (d(j)-c(j)*x(j+1))/b(j)
+        U(j) = (d(j)-c(j)*U(j+1))/b(j)
       end do
-!      do j=1,N
-!        write(*,*) "x(",j,") = ",x(j)
-!      end do
+do j=0,N-1
+ write(1,*) "x(",j,") = ",x(j),f(j),Uex(j),U(j)
+end do
     END SUBROUTINE Thomas
     SUBROUTINE error(j,M,U,Uex,dx)
       INTEGER,INTENT(INOUT) :: j
       INTEGER,INTENT(IN) :: M
       REAL*8,INTENT(INOUT) :: dx
       REAL*8,DIMENSION(0:1000),INTENT(INOUT) :: U,Uex
-      REAL*8 :: e=0
+      REAL*8 :: e
+      e=0
       do j=0,M
 	e = e + (abs(Uex(j)-U(j)))**2
       end do
       e = (e*dx)**.5
+!      write(1,*) dx/pi,e
+    END SUBROUTINE error
+END PROGRAM Newmann
